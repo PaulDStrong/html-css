e7fec39e3ff3a2ee31f5e20788069c3650e0ee1d
PaulDStrong <umarth@gmail.com>
2016-01-23 11:53:26 -0800

    A Few Files

---
+++ faqTemplate.php
@@ -0,0 +1,41 @@
+<?php
+/*
+template name: FAQ Page
+*/
+?>
+	
+
+
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+
+<html>
+<title>FAQ</title>
+
+<body>
+<p> <strong><a name"top">FREQUENTLY ASKED QUESTIONS</a></strong></p>
+<p>&nbsp;</p
+<p>1. <a href ="#Question1">Why didn't FileZilla work? </p>
+<p>2. <a href="#Question2"> Why can't I get a FAQ page ordered correctly? </p>
+<p>3. <a href="#Question3">Will I have to keep downloading and updating?</p>
+    
+
+<br><br>
+</p>
+<p><strong> Answers </strong></p>
+
+<p><a name="Question1">1. Turns out, you downloaded the server and then spent a good amount of time lookign through various instructions sets in an attempt to fix the impossible.  Then you got a little smarter, downloaded the right application for a client and things became much easier. </a></p>
+<br><br>
+<a href="#top">Top</a>
+<p><a name="Question2">2. Because you are using a really crappy template that you are spending more time fixing than you would have if you had started from scratch.  On the plus side, you did learn how to hyper link to differnt content areas on an html file, which will come in handy if you ever get around to designing your own site.</a></p>
+
+ <br><br>
+   <a href="#top">Top</a>
+   <p><a name="Question3">3. Let's give it a shot... </a></p>
+<br><br>
+<a href="#top">Top</a>
+</body>
+</html>
